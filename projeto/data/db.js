98836986d3d443a2fa8e3ab7bd78129c88d7f73a
jackesfonseca <jackesdafonseca@gmail.com>
2020-06-17 14:36:48 -0300

    criacao de uma consulta do tipo enum

--- projeto/data/db.js
+++ projeto/data/db.js
@@ -3,19 +3,22 @@ const users = [{
     name: 'Bob',
     email: 'bob@gmail.com',
     age: 30,
-    profile_id: 2
+    profile_id: 2,
+    status: 'ACTIVE'
 }, {
     id: 2,
     name: 'Ana',
     email: 'ana@gmail.com',
     age: 16,
-    profile_id: 1    
+    profile_id: 1,   
+    status: 'INACTIVE' 
 }, {
     id: 3,
     name: 'Dean',
     email: 'dean@gmail.com',
     age: 20,
-    profile_id: 1
+    profile_id: 1,
+    status: 'BLOCKED'
 }]
 
 const profiles = [
