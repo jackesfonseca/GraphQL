ecd6692a638b175bceac7135d0f6c174aa9b9bdc
jackesfonseca <jackesdafonseca@gmail.com>
2020-07-07 18:58:28 -0300

    criacao da migration de usuarios

--- banco-de-dados/projeto-database/knexfile.js
+++ banco-de-dados/projeto-database/knexfile.js
@@ -1,16 +1,16 @@
 // Update with your config settings.
+const {connection} = require ('./.env')
 
-const {connection} = require('./.env') //As informações com o banco de daos foram externalizadas
 module.exports = {
 
-  client: 'mysql',
-  connection,
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
+    client: 'mysql',
+    connection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
 
 };
